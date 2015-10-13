f73f8fc628dd10629762e9ded4f42fd8b786f6ea
Nick Welna <welnanick@gmail.com>
2015-10-13 14:10:46 -0500

    Initial mod file created, after initial setup, branches will be used.

---
+++ 1.7.10/src/main/java/com/taji34/troncraft/Troncraft.java
@@ -0,0 +1,38 @@
+package com.taji34.troncraft;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Nick on 10/13/2015.
+ */
+
+@Mod(modid = Troncraft.MODID, name = Troncraft.MODNAME, version = Troncraft.VERSION)
+public class Troncraft {
+
+    public static final String MODID = "troncraft";
+    public static final String MODNAME = "Troncraft";
+    public static final String VERSION = "1.0.0";
+
+    @Instance(value = Troncraft.MODID)
+    public static Troncraft instance;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void load(FMLInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
